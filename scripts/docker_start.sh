4f3b54a9ad63938bfd433e60d7bd11ee1a413633
Songtao Cheng <songtao.cheng@scilifelab.se>
2025-05-19 21:06:25 +0200

    update workspace

--- scripts/docker_start.sh
+++ scripts/docker_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 npm run build --prefix frontend
-python -m agent_lens connect-server --server_url="https://hypha.aicell.io" --workspace_name="agent-lens"
+python -m agent_lens connect-server --server_url="https://hypha.aicell.io"
